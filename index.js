307a8584de84ff272c3328e5924a686303002e41
Lam Ho Ming <lamhoming112@gmail.com>
2022-04-10 18:47:28 +0800

    remove file

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-// import App from './test'
 import App from './main/BLEScanner';
 import {name as appName} from './app.json';
 
